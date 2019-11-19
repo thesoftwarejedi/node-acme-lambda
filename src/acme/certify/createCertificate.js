d3c8f275441450751405453bb881607e77d659c0
Dana Hanna <dana.hanna@gmail.com>
2019-11-18 22:20:57 -0500

    save pems

--- src/acme/certify/createCertificate.js
+++ src/acme/certify/createCertificate.js
@@ -4,7 +4,7 @@ const generateCSR = require('../../util/generateCSR')
 const config = require('../../../config')
 const saveFile = require('../../aws/s3/saveFile')
 
-const saveCertificate = (data) =>
+const saveCertificate = (data) => {
   saveFile(
     config['s3-cert-bucket'],
     config['s3-folder'],
@@ -14,7 +14,33 @@ const saveCertificate = (data) =>
       cert: data.cert,
       issuerCert: data.issuerCert
     })
-  )
+  );
+  console.log(`About to write PEM files for ${key}..`)
+  try {
+    saveFile(
+      config['s3-cert-bucket'],
+      config['s3-folder'],
+      `${data.key}.pem`,
+      data.cert
+    );
+    if (certJSON.issuerCert) {
+      saveFile(
+        config['s3-cert-bucket'],
+        config['s3-folder'],
+        `${data.key}-chain.pem`,
+        data.issuerCert.toString()
+      );
+    }
+    saveFile(
+      config['s3-cert-bucket'],
+      config['s3-folder'],
+      `${data.key}-key.pem`,
+      data.keypair.privateKeyPem.toString()
+    );
+  } catch (e) {
+    console.error('Error writing pem files', e)
+  }
+}
 
 const createCertificate = (certUrl, certInfo, acctKeyPair) => (authorizations) =>
   generateRSAKeyPair()
